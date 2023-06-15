1d8414010624a86997dcffc8d47c0b46c75eb5cd
GeorgeDaris <george.darisgr@gmail.com>
2023-06-15 23:54:11 +0300

    Created reusable directives for handling clicks outside of an element, and for focusing on elements.

---
+++ src/directives/clickOutside.js
@@ -0,0 +1,16 @@
+export const ClickOutside = {
+  mounted(el, binding) {
+    el.__ClickOutsideHandler__ = (event) => {
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event);
+      }
+    };
+    setTimeout(
+      () => document.body.addEventListener("click", el.__ClickOutsideHandler__),
+      0
+    );
+  },
+  unmounted(el) {
+    document.body.removeEventListener("click", el.__ClickOutsideHandler__);
+  },
+};
