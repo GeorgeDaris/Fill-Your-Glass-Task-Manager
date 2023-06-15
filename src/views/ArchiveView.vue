cc30a17f1456a00a63bfebc05aa91486934b3b6d
GeorgeDaris <george.darisgr@gmail.com>
2023-06-15 23:59:30 +0300

    Testing and cleanup

--- src/views/ArchiveView.vue
+++ src/views/ArchiveView.vue
@@ -1,11 +1,7 @@
 <script setup>
 import ArchiveEntries from "../components/ArchiveEntries.vue";
-// import Home from "../components/TaskView.vue";
-// let test = ref("test");
 </script>
 
 <template>
-  <!-- {{ test }} -->
-  <!-- <Home /> -->
   <ArchiveEntries></ArchiveEntries>
 </template>
