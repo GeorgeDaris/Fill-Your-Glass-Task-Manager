eaba5ef2361965ae5a8a778e9475713c8a9746ef
GeorgeDaris <george.darisgr@gmail.com>
2023-06-16 00:00:38 +0300

    Composable for turning months into numbers

--- src/composables/monthToNumber.js
+++ src/composables/monthToNumber.js
@@ -1,3 +1,4 @@
+//turns a month's name into a number (E.g February = 2)
 export function useMonthToNumber(month) {
   let monthNum;
   monthNum = new Date(Date.parse(month + "1, 2001")).getMonth() + 1;
