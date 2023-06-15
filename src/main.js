44110949465ad3ce05f6bfef7d18d3857548b482
GeorgeDaris <george.darisgr@gmail.com>
2023-06-15 23:56:24 +0300

    Using the directives

--- src/main.js
+++ src/main.js
@@ -2,10 +2,16 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import { ClickOutside } from "./directives/clickOutside";
+import { vFocus } from "./directives/focus";
+
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
+
+app.directive("click-outside", ClickOutside);
+app.directive("focus", vFocus);
 
 app.mount("#app");
