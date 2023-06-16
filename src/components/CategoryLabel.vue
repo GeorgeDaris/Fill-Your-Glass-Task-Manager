1a62ecbef93ff6736b8a49e9d56750cdb55dc428
GeorgeDaris <george.darisgr@gmail.com>
2023-06-16 10:28:49 +0300

    Improved accessibility by using a span instead of a div inside the label

--- src/components/CategoryLabel.vue
+++ src/components/CategoryLabel.vue
@@ -23,20 +23,21 @@ defineProps(["labelColor", "hasSubTasks", "isRadioButton"]);
     </div>
   </div>
 
-  <div
+  <span
     v-else-if="isRadioButton"
-    class="group scale-[0.82] ml-2 relative hover:cursor-pointer label-container"
+    class="group block scale-[0.82] ml-2 relative hover:cursor-pointer label-container flex-shrink-0 flex-grow-0"
     :style="{ '--label-color': labelColor }"
   >
-    <div
+    <!-- Finally solved this bug where the labels would appear squashed on firefox. It was due to flex-shrink! OR maybe not... FIXED: Turns out the container <li> needed a min-width -->
+    <span
       class="string inline-block relative w-4 h-[1.55rem] border-[0.05rem] border-textColor border-b-[transparent] rounded-[70%] -mb-[0.8rem] z-[2] transition-all duration-300 after:content-[''] after:w-[0.52rem] after:h-[0.5rem] after:bg-[var(--label-color)] after:absolute after:bottom-[0.13rem] after:-left-[0.2rem] after:rotate-[33deg] dark:border-bgColor group-hover:-rotate-[3deg]"
     >
       <!-- <slot name="input" /> -->
-    </div>
+    </span>
     <slot name="input" />
 
     <!-- shadow-[0.2rem_0.2rem_0px_0px_#ffffff,0.3rem_0.3rem_0px_0px_var(--accent)] dark:shadow-[0.2rem_0.2rem_0px_0px_#1d212a -->
-    <div
+    <span
       class="label inline-block text-bgColor p-1 pt-6 relative rounded-sm origin-top ,0.3rem_0.3rem_0px_0px_var(--accent)] peer-checked:shadow-[0.2rem_0.2rem_0px_0px_#ffffff,0.3rem_0.3rem_0px_0px_var(--accent)] dark:peer-checked:shadow-[0.2rem_0.2rem_0px_0px_#1d212a,0.3rem_0.3rem_0px_0px_var(--accent)] transition-all duration-300 after:content-[''] after:block after:absolute after:left-0 after:top-0 after:width-0 after:border-t-solid after:border-b-[0.8rem] after:border-b-[var(--label-color)] after:border-l-solid after:border-l-[0.7rem] after:border-l-bgColor before:content-[''] before:block before:absolute before:right-0 before:top-0 before:width-0 before:border-b-solid before:border-b-[0.8rem] before:border-b-[var(--label-color)] before:border-r-solid before:border-r-[0.7rem] before:border-r-bgColor hover:transition-all hover:duration-300 before:dark:border-r-darkBg after:dark:border-l-darkBg group-hover:-rotate-[3deg] group-hover:origin-top"
       :style="{ 'background-color': labelColor, '--label-color': labelColor }"
     >
@@ -45,8 +46,8 @@ defineProps(["labelColor", "hasSubTasks", "isRadioButton"]);
       >
         <slot name="name" />
       </span>
-    </div>
-  </div>
+    </span>
+  </span>
 
   <div
     v-else
