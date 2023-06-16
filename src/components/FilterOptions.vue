06efa4e3cfb5f4cfe5727c04082935aaa4280526
GeorgeDaris <george.darisgr@gmail.com>
2023-06-16 10:51:50 +0300

    Filtering of entries based on categories added

--- src/components/FilterOptions.vue
+++ src/components/FilterOptions.vue
@@ -9,20 +9,28 @@ let categories = inject("categories");
 let selectedCategory = ref(categories[0].title);
 
 const sendFilter = () => {
-  //emitting the filter option to the paretn: ArchiveEntry
-  emit("filter", option.value);
+  //emitting the filter option to the parent: ArchiveEntry
+  if (option.value === "Category") {
+    //sending the category when specified
+    emit("filter", option.value, selectedCategory.value);
+  } else {
+    emit("filter", option.value);
+  }
 };
 </script>
 
 <template>
-  <form @submit.prevent class="mt-4 ml-8 flex items-center gap-6">
-    <label for="">Filter by </label>
+  <form
+    @submit.prevent
+    class="md:mt-8 ml-4 md:ml-40 px-2 flex flex-col md:items-center md:flex-row gap-2 md:gap-6"
+  >
+    <label for="filterOption">Filter by </label>
     <select
       v-model="option"
       @change="sendFilter()"
-      name=""
-      id=""
-      class="bg-bgColor text-text max-w-[8rem] p-2 border-2 border-accentColor rounded-md cursor-pointer dark:bg-darkBg dark:text-darkText"
+      name="filterOption"
+      id="filterOption"
+      class="bg-bgColor text-text mr-4 md:max-w-[8rem] p-2 border-2 border-accentColor rounded-md cursor-pointer dark:bg-darkBg dark:text-darkText"
     >
       <option value="Most recent">Most recent</option>
       <option value="Oldest">Oldest</option>
@@ -31,12 +39,13 @@ const sendFilter = () => {
 
     <!-- Displaying options once the user has chosen to filter by categories -->
     <template v-if="option === 'Category'">
-      <label for="">{{ option }} </label>
+      <label for="categoryOption">{{ option }} </label>
       <select
         v-model="selectedCategory"
-        name=""
-        id=""
-        class="bg-bgColor text-text max-w-[8rem] p-2 border-2 border-accentColor rounded-md cursor-pointer dark:bg-darkBg dark:text-darkText"
+        @change="sendFilter()"
+        name="categoryOption"
+        id="categoryOption"
+        class="bg-bgColor text-text mr-4 md:max-w-[8rem] p-2 border-2 border-accentColor rounded-md cursor-pointer dark:bg-darkBg dark:text-darkText"
       >
         <option
           v-for="category in categories"
