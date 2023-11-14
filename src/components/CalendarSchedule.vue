9a8b2503085ccf5bed66c6e2eba1a7a8497c589f
GeorgeDaris <george.darisgr@gmail.com>
2023-11-14 13:26:27 +0200

    Calendar gird column fix

--- src/components/CalendarSchedule.vue
+++ src/components/CalendarSchedule.vue
@@ -69,7 +69,7 @@ const sendTodo = (task, date) => {
 
 <template>
   <div
-    class="grid relative mx-4 md:ml-16 gap-10 md:grid-cols-[2fr_1fr] 2xl:w-[90vw] 2xl:grid-cols[2fr_auto] 2xl:gap-[5%]"
+    class="grid relative mx-4 md:ml-16 gap-10 md:grid-cols-[2fr_1fr] 2xl:w-[90vw] 2xl:grid-cols-[2fr_auto] 2xl:gap-[5%]"
   >
     <transition name="todos" appear>
       <CalendarElement
