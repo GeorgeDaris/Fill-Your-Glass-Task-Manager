3119ebd8ad6bfa9fcc92fc283f881f50d9ad5631
GeorgeDaris <george.darisgr@gmail.com>
2023-11-15 11:24:57 +0200

    Correct month abbreviations

--- src/components/DateIcon.vue
+++ src/components/DateIcon.vue
@@ -17,10 +17,19 @@ const dateTimeString = computed(() => {
 // truncating the month name for mobile devices
 const truncatedMonth = computed(() => {
   let width = window.innerWidth > 0 ? window.innerWidth : screen.Width;
-  if (width < 768) {
-    return props.date.month.substring(0, 3);
+  const month = props.date.month;
+  const monthNum = useMonthToNumber(month);
+  // May, June, July.
+  const unabbreviatedMonths = [5, 6, 7];
+  if (width < 768 && !unabbreviatedMonths.includes(monthNum)) {
+    //September gets abbreviated with an extra letter: Sept.
+    if (monthNum == 9) {
+      return `${month.substring(0, 4)}.`;
+    } else {
+      return `${month.substring(0, 3)}.`;
+    }
   } else {
-    return props.date.month;
+    return month;
   }
 });
 </script>
@@ -36,7 +45,7 @@ const truncatedMonth = computed(() => {
           class="flex md:block after:content-[''] after:flex-1 w-full h-full px-2 py-[0.1rem] md:p-2 rounded-md border-[1px] border-b-2 border-lightGrey dark:border-lightDark bg-gradient-to-r md:bg-gradient-to-b from-accentColor to-bgColor from-[25%] to-[25%] md:from-[40.5%] md:to-[40.5%] dark:to-darkBg dark:from-[25%] dark:to-[25%] dark:md:from-[40.5%] dark:md:to-[40.5%]"
         >
           <span
-            class="uppercase flex-1 self-center text-left md:text-[0.7rem] tracking-widest inline-block after:content-['\00a0\00a0'] md:after:content-[''] dark:text-darkBg"
+            class="md:uppercase flex-1 self-center text-left md:text-[0.7rem] tracking-widest inline-block after:content-['\00a0\00a0'] md:after:content-[''] dark:text-darkBg"
             >{{ truncatedMonth }}
           </span>
 
